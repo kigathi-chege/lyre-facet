2e33f7de39c5a36d38be990b91f55f6d5b0dd6c9
kigathi-chege <chegekigathi@gmail.com>
2025-05-06 02:21:46 +0300

    feat: ensure all requests are stateful

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function () {
-    Route::apiResources([
-        'facets' => \Lyre\Facet\Http\Controllers\FacetController::class,
-        'facetvalues' => \Lyre\Facet\Http\Controllers\FacetValueController::class,
-    ]);
-});
+Route::prefix('api')
+    ->middleware(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class)
+    ->group(function () {
+        Route::apiResources([
+            'facets' => \Lyre\Facet\Http\Controllers\FacetController::class,
+            'facetvalues' => \Lyre\Facet\Http\Controllers\FacetValueController::class,
+        ]);
+    });
